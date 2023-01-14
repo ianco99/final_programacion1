4e8c3981d9b20a7b5ad0242f51c3035375c1fbc1
Ian Kuznicki <iankuz25@gmail.com>
2023-01-14 16:25:16 -0300

    Finished base entity class

--- src/GameObjects/BaseClasses/BaseEntity.h
+++ src/GameObjects/BaseClasses/BaseEntity.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "AwesomeLibrary.h"
+#include "Utilities.h"
 
 class BaseEntity
 {
 private:
-	
+	Vector2 body;
+
+	Vector2 position;
+	Vector2 direction;
+	int velocity;	//Windows console works with entire pixels, instead of positions with floating point values
+
+	int health;
+	int damage;		//Damage dealt to other body on collision
 };
