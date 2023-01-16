d694e9ecb1bbb5c3f9b6af32b3eddcddfe9bf157
Ian Kuznicki <iankuz25@gmail.com>
2023-01-16 12:22:20 -0300

    Started RunGame class

--- src/System/RunGame.h
+++ src/System/RunGame.h
@@ -1,1 +1,42 @@
 #pragma once
+#include "GameObjects/Player.h"
+#include "GameObjects/Asteroid.h"
+
+enum GameStates
+{
+	Playing,
+	Win,
+	Lost
+};
+
+struct GameConfigs
+{
+	static GameConfigs instance;
+	static const int maxAsteroids = 20;
+};
+
+class RunGame
+{
+private:
+	Player* player;
+	//Asteroid asteroids[GameConfigs.maxAsteroids];
+
+
+public:
+	RunGame();
+	~RunGame();
+
+	void Update();
+	void CheckInput();
+	void MoveEntities();
+	void CheckCollisions();
+
+};
+
+//RunGame::RunGame()
+//{
+//}
+//
+//RunGame::~RunGame()
+//{
+//}
